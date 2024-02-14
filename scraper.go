0325cd8b956419dda8d379c7ef752db6b35e5ba7
gavinmcfall <gavin@nerdz.co.nz>
2024-02-14 20:17:49 +1300

    refactor(PIAS): Pulling age ratings from IMDB

--- scraper.go
+++ scraper.go
@@ -4,36 +4,65 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 // initializing a data structure to keep the scraped data
-type PokemonProduct struct {
-	url, image, name, price string
+type videoAgeRating struct {
+	Country string
+	Rating  string
+	IMDbID  string
 }
 
 func main() {
 	// initializing the slice of structs to store the data to scrape
-	var pokemonProducts []PokemonProduct
+	var videoAgeRatings []videoAgeRating
+
+	// Extract IMDb ID from the URL
+	imdbURL := "https://www.imdb.com/title/tt15314262/parentalguide"
+	imdbIDParts := strings.Split(imdbURL, "/")
+	var imdbID string
+	for i, part := range imdbIDParts {
+		if part == "title" && i+1 < len(imdbIDParts) {
+			imdbID = imdbIDParts[i+1]
+			break
+		}
+	}
 
 	// creating a new Colly instance
 	c := colly.NewCollector()
 
 	// scraping logic
-	c.OnHTML("li.product", func(e *colly.HTMLElement) {
-		pokemonProduct := PokemonProduct{}
+	c.OnHTML("section#certificates", func(e *colly.HTMLElement) {
+		e.ForEach("ul.ipl-inline-list li.ipl-inline-list__item a", func(_ int, elem *colly.HTMLElement) {
+			// Extract text from the <a> element
+			text := elem.Text
 
-		pokemonProduct.url = e.ChildAttr("a", "href")
-		pokemonProduct.image = e.ChildAttr("img", "src")
-		pokemonProduct.name = e.ChildText("h2")
-		pokemonProduct.price = e.ChildText(".price")
+			// Split the text by colon
+			parts := strings.Split(text, ":")
 
-		pokemonProducts = append(pokemonProducts, pokemonProduct)
+			// Ensure there are two parts (country and rating)
+			if len(parts) == 2 {
+				country := strings.TrimSpace(parts[0])
+				rating := strings.TrimSpace(parts[1])
+
+				// Create a new videoAgeRating struct
+				videoAgeRating := videoAgeRating{
+					Country: country,
+					Rating:  rating,
+					IMDbID:  imdbID,
+				}
+
+				// Append the struct to the slice
+				videoAgeRatings = append(videoAgeRatings, videoAgeRating)
+			}
+		})
 	})
 
 	// visiting the target page
-	c.Visit("https://scrapeme.live/shop/")
+	c.Visit(imdbURL)
 
 	// opening the CSV file
 	file, err := os.Create("products.csv")
@@ -47,21 +76,19 @@ func main() {
 
 	// writing the CSV headers
 	headers := []string{
-		"url",
-		"image",
-		"name",
-		"price",
+		"IMDbID",
+		"Country",
+		"Rating",
 	}
 	writer.Write(headers)
 
-	// writing each Pokemon product as a CSV row
-	for _, pokemonProduct := range pokemonProducts {
-		// converting a PokemonProduct to an array of strings
+	// writing each videoAgeRating as a CSV row
+	for _, videoAgeRating := range videoAgeRatings {
+		// converting a videoAgeRating to an array of strings
 		record := []string{
-			pokemonProduct.url,
-			pokemonProduct.image,
-			pokemonProduct.name,
-			pokemonProduct.price,
+			videoAgeRating.IMDbID,
+			videoAgeRating.Country,
+			videoAgeRating.Rating,
 		}
 
 		// adding a CSV record to the output file
